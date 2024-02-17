b2bb4a5e56a98d1698ca00d54af48d043bf465c6
kamal chanchal <kchanchal78@gmail.com>
2024-02-17 15:37:34 +0530

    Add project files.

---
+++ BlackScholesModel/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace BlackScholesModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(724, 554);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(10, 37);
+            this.panel1.Margin = new System.Windows.Forms.Padding(10);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(10);
+            this.panel1.Size = new System.Drawing.Size(704, 507);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Price (S) :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Strike Price (K):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Rate Of Intrest (R)  :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(266, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Time of Expiry (Days - T)  :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(22, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(219, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Implied Volatility (V) :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(22, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 28);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Call /Put Price :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(286, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 34);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(289, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(390, 34);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(291, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(390, 34);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(293, 200);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(390, 34);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(290, 264);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(390, 34);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(292, 382);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(390, 34);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox6_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(329, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 54);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(32, 329);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(159, 28);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Select Call Put :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(294, 326);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 36);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(37, 441);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 54);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 569);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "BSM Quant";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BlackScholesModel/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BlackScholesModel
+{
+    public partial class Form1 : Form
+    {
+        //Ref https://www.codearmo.com/python-tutorial/options-trading-black-scholes-model
+
+        double S_CurrentPrice = 0;
+        double K_StrikePrice = 0;
+        double R_Rate_of_intrest = 0;
+        int T_TimeofExpiry = 0;
+        double IV_ImpliedVolatiity = 0;
+        string CallPut_selection = String.Empty;
+        double Output_model =0;
+        public Form1()
+        {
+            InitializeComponent();
+            StopformResizing();
+            this.AdditemtoCombobox();
+            FillDefaultperiod();
+
+        }
+
+        private void StopformResizing()
+        {
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+
+            //Application Name
+            //this.Name = "Quant : Calculate Black Scholes using c# (.NET Framework) ";
+            this.Text = "Quant : Calculate Black Scholes using c# (.NET Framework) ";
+        }
+        private void AdditemtoCombobox()
+        {
+            this.comboBox1.Items.Add("CE");
+            this.comboBox1.Items.Add("PE");
+        }
+        private void FillDefaultperiod()
+        {
+            try
+            {
+                this.S_CurrentPrice = 100;
+                this.K_StrikePrice = 100;
+                this.R_Rate_of_intrest = 0.1;
+                this.T_TimeofExpiry = 1;
+                this.IV_ImpliedVolatiity = 0.3;
+
+                this.textBox1.Text = this.S_CurrentPrice.ToString();
+                this.textBox2.Text = this.K_StrikePrice.ToString();
+                this.textBox3.Text = this.R_Rate_of_intrest.ToString();
+                this.textBox4.Text = this.T_TimeofExpiry.ToString();
+                this.textBox5.Text = this.IV_ImpliedVolatiity.ToString();
+                this.textBox6.Text = "NA";
+                this.comboBox1.Text = "Select";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(" Error(s) ");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!IsvalidInput())
+            {
+                MessageBox.Show(" Error(s) Please Enter Valid Input");
+            }
+            GetInput_datapoint();
+
+            StartProcessingBSM();
+        }
+
+        private void GetInput_datapoint()
+        {
+            this.S_CurrentPrice = Convert.ToDouble( this.textBox1.Text);
+            this.K_StrikePrice = Convert.ToDouble(this.textBox2.Text)  ;
+            this.R_Rate_of_intrest= Convert.ToDouble(this.textBox3.Text) ;
+            this.T_TimeofExpiry = Convert.ToInt32(this.textBox4.Text);
+            this.IV_ImpliedVolatiity = Convert.ToDouble(this.textBox5.Text);
+
+        }
+
+        private bool IsvalidInput()
+        {
+            bool result = false;
+
+            try
+            {
+                if(textBox1.Text is null)
+                {
+                    return result;
+                }
+
+                if (textBox2.Text is null)
+                {
+                    return result;
+                }
+
+                if (textBox3.Text is null)
+                {
+                    return result;
+                }
+
+                if (textBox4.Text is null)
+                {
+                    return result;
+                }
+
+                if (textBox5.Text is null)
+                {
+                    return result;
+                }
+
+                if (textBox6.Text is null)
+                {
+                    return result;
+                }
+                if(comboBox1.SelectedItem is null)
+                {
+                    return result;
+                }
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return result;
+    }
+
+        private void StartProcessingBSM()
+        {
+            if (this.comboBox1.SelectedItem is null)
+                return;
+
+            string Selectedcallput = this.comboBox1.SelectedItem.ToString();
+
+            if (Selectedcallput is null)
+                return;
+
+            double result = -1;
+
+            if (Selectedcallput.Equals("CE"))
+            {
+                result = Call_bsm(this.S_CurrentPrice, this.K_StrikePrice, this.T_TimeofExpiry, this.R_Rate_of_intrest, this.IV_ImpliedVolatiity);
+            }
+            else if (Selectedcallput.Equals("PE"))
+            {
+                result =  put_bsm(this.S_CurrentPrice, this.K_StrikePrice, this.T_TimeofExpiry, this.R_Rate_of_intrest, this.IV_ImpliedVolatiity);
+            }
+
+            if (result !=  -1)
+            {
+                this.textBox6.Text =result.ToString();  
+            }
+            else
+            {
+                this.textBox6.Text = "Error";
+                MessageBox.Show("Error ! Please verify the Input");
+            }
+        }
+
+        #region SECTION : Implement Black Schole Model
+
+        
+        private double Call_bsm( double StockPrice, double StrikePrice , int TimePeriod , double RateofIntrest , double roh_Volatility  )
+        {
+            //ref https://www.codearmo.com/python-tutorial/options-trading-black-scholes-model
+            double model = -1;
+            try
+            {
+                //d1 denote Get 
+                double d1 = (Math.Log(StockPrice / StrikePrice) + (RateofIntrest + Math.Pow(roh_Volatility, 2) / 2) * TimePeriod) / (roh_Volatility * Math.Sqrt(TimePeriod));
+
+                //d2 denote Pay 
+                double d2 =  d1 - roh_Volatility * Math.Sqrt(TimePeriod);
+
+                model = StockPrice * CDF(d1) - StrikePrice * Math.Exp(-RateofIntrest * TimePeriod) * CDF(d2);
+                return model;
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine($"Error Ocurred(s) {ex.Message}");
+            }
+            return model;
+        }
+
+        private double put_bsm(double StockPrice, double StrikePrice, int TimePeriod, double RateofIntrest, double roh_Volatility)
+        {
+            //ref https://www.codearmo.com/python-tutorial/options-trading-black-scholes-model
+            double model = -1;
+            try
+            {
+                //d1 denote Get 
+                double d1 = (Math.Log(StockPrice / StrikePrice) + (RateofIntrest + Math.Pow(roh_Volatility, 2) / 2) * TimePeriod) / (roh_Volatility * Math.Sqrt(TimePeriod));
+
+                //d2 denote Pay 
+                double d2 = d1 - roh_Volatility * Math.Sqrt(TimePeriod);
+
+                model = StrikePrice * Math.Exp(-RateofIntrest * TimePeriod) * CDF(-d2) - (StockPrice * CDF( -d1) );
+                return model;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error Ocurred(s) {ex.Message}");
+            }
+            return model;
+        }
+
+        private double CDF(double x)
+        {
+
+            //ref https://www.johndcook.com/blog/csharp_phi/
+
+            // constants
+            double a1 = 0.254829592;
+            double a2 = -0.284496736;
+            double a3 = 1.421413741;
+            double a4 = -1.453152027;
+            double a5 = 1.061405429;
+            double p = 0.3275911;
+
+            // Save the sign of x
+            int sign = 1;
+            if (x < 0)
+                sign = -1;
+            x = Math.Abs(x) / Math.Sqrt(2.0);
+
+            // A&S formula 7.1.26
+            double t = 1.0 / (1.0 + p * x);
+            double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
+
+            return 0.5 * (1.0 + sign * y);
+        }
+
+        #endregion
+
+
+        #region : SECTION CONTROL EVENTS
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FillDefaultperiod();
+        }
+        #endregion
+    }
+}
